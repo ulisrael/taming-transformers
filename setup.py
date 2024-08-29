81feeca3dd9ed81839cf79a7953feaf305de7008
Uriah Israel <ulisrael@gmail.com>
2024-08-29 11:54:06 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,23 @@ setup(
     description='Taming Transformers for High-Resolution Image Synthesis',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/ulisrael/taming-transformers',
+    url='https://github.com/ulisrael/taming-transformers',  # Replace with the correct URL
     author='Your Name',
     author_email='your.email@example.com',
-    packages=find_packages(),  # Automatically find all packages and sub-packages
+    packages=find_packages(),
     install_requires=[
-        'torch',
-        'numpy',
-        'tqdm',
+        'torch==1.7.0',
+        'torchvision==0.8.1',
+        'transformers==4.3.1',
+        'sacrebleu==1.5.1',
+        'packaging==20.9',
+        'filelock==3.0.12',
+        'numpy==1.19.5',
+        'tokenizers==0.10.1',
+        'sacremoses==0.0.43',
+        'tqdm==4.49.0',
+        'regex==2020.11.13',
+        'requests==2.24.0'
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
