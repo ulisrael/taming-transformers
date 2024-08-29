3979f1b673e59b784c334a84f46c99720803bfad
Uriah Israel <ulisrael@gmail.com>
2024-08-29 11:50:20 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-
 from setuptools import setup, find_packages
 
 setup(
     name='taming-transformers',
     version='0.0.1',
     description='Taming Transformers for High-Resolution Image Synthesis',
-    long_description=open('README.md').read(),  # Assuming you have a README.md file
-    long_description_content_type='text/markdown',  # Specifies the content type of the long description
-    url='https://github.com/yourusername/taming-transformers',  # Replace with your repository URL
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ulisrael/taming-transformers',
     author='Your Name',
     author_email='your.email@example.com',
-    packages=find_packages(include=["taming", "taming.*"]),
+    packages=find_packages(),  # Automatically find all packages and sub-packages
     install_requires=[
         'torch',
         'numpy',
@@ -18,10 +17,10 @@ setup(
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',  # Change to your license
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',  # Specify the Python version requirement
+    python_requires='>=3.6',
 )
 
 # from setuptools import setup, find_packages
